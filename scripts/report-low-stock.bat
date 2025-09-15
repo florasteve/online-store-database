dc48e99f5c2e71f6999fd1ca5224989f09b3944f
Steve Flora <slflora96@gmail.com>
2025-09-15 11:28:56 -0400

    fix: low-stock view & CSV script
    Removed ORDER BY from view (sort in consumer). Replaced corrupted report-low-stock.bat; outputs ordered CSV.

--- scripts/report-low-stock.bat
+++ scripts/report-low-stock.bat
@@ -2,6 +2,9 @@
 setlocal
 set "OUT=data\low_stock_report.csv"
 echo ProductID,Name,StockQuantity> "%OUT%"
-docker exec -i store-mssql /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U SA -P "YourStrong!Passw0rd" -d OnlineStoreDB -h -1 -W -s , -Q "SET NOCOUNT ON; SELECT ProductID, Name, StockQuantity FROM dbo.vwLowStock;" >> "%OUT%"
+docker exec -i store-mssql /opt/mssql-tools18/bin/sqlcmd ^
+  -C -S localhost -U SA -P "YourStrong!Passw0rd" -d OnlineStoreDB ^
+  -h -1 -W -s , ^
+  -Q "SET NOCOUNT ON; SELECT ProductID, Name, StockQuantity FROM dbo.vwLowStock ORDER BY StockQuantity ASC, Name ASC;" >> "%OUT%"
 echo Wrote %OUT%
 endlocal
