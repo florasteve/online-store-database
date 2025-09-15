27e7a4d7d5db76a359498524a407b0aae9eba220
Steve Flora <slflora96@gmail.com>
2025-09-15 19:30:24 -0400

    feat(security): least-privileged appuser
    Added appuser login/user and least-privileged grants (EXEC on dbo procs; SELECT on reporting views). Keeps tables locked down and aligns with principle of least privilege.

---
+++ sql/13_security.sql
@@ -0,0 +1,23 @@
+USE master;
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.sql_logins WHERE name = N'appuser')
+  CREATE LOGIN appuser WITH PASSWORD = 'App$trongPassw0rd!';
+GO
+
+USE OnlineStoreDB;
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = N'appuser')
+  CREATE USER appuser FOR LOGIN appuser;
+GO
+
+-- EXECUTE on procs in dbo (adjust if you add other schemas)
+GRANT EXECUTE ON SCHEMA::dbo TO appuser;
+GO
+
+-- Select on reporting views only (explicit, avoids table access)
+GRANT SELECT ON OBJECT::dbo.vwOrderSummary TO appuser;
+GRANT SELECT ON OBJECT::dbo.vwBestSellers30D TO appuser;
+GRANT SELECT ON OBJECT::dbo.vwOrderSummaryWithStatus TO appuser;
+GRANT SELECT ON OBJECT::dbo.vwLowStock TO appuser;
+GRANT SELECT ON OBJECT::dbo.vwCategorySales30D TO appuser;
+GO
